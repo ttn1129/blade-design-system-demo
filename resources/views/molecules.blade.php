53d4f07767111c95158b8cada6e8351ba9f1f025
ttn1129 <ttnsky1129@yahoo.co.jp>
2019-12-04 13:44:04 +0900

    20191204_boobooksのheaderを取り込む

--- resources/views/molecules.blade.php
+++ resources/views/molecules.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 molcules
-@component('components.molecules.header-logo', [
+@component('components.molecules.header', [
     'title'=>'タイトル in normal-block',
     'subtitle'=>'サブタイトル in normal-block',
     ])
